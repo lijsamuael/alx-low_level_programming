4f04be6d567423e3b370fdcd2c49ce50f42eec92
lijsamuael <lijsamuael@gmail.com>
2022-04-10 00:52:35 +0300

    getting stacked with betty

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,14 +14,17 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-
 if (n > 1)
+{
 printf("%d is positive\n", n);
+}
 else if (n < 0)
+{
 printf("%d is negative\n", n);
+}
 else
-
+{
 printf("%d is zero\n", n);
-
+}
 return (0);
 }
