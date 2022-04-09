ba31cd93c84dafcd8fe7cd68c447d33e0b27ae43
lijsamuael <lijsamuael@gmail.com>
2022-04-10 01:24:36 +0300

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Description: Print the lowercase alphabet
+ *Return: 0
+ */
+int main(void)
+{
+int c;
+
+for (c = 0; c < 26; ++c)
+putchar('a' + c);
+
+putchar('\n');
+
+return (0);
+}
