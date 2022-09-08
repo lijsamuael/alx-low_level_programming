c12373b0e1e549866143b2a8efe8c2a8a7e7d159
lijsamuael <lijsamuael@gmail.com>
2022-09-08 08:11:51 -0700

    adding the last advanced task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main function
+ *
+ * with return error 1
+ */
+int main(void)
+{
+fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (0);
+}
