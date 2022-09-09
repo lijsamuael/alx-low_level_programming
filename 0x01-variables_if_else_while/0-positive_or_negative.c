8d2e12cea3978f5c6412eb3c5cd8161153149f98
lijsamuael <lijsamuael@gmail.com>
2022-09-08 23:30:29 -0700

    adding task 9

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,26 +1,21 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+#include <time.h>
 
 /**
- * main - Prints a random number and states whether
- *         it is positive, negative, or zero.
+ * main - Entry point
  *
- * Return: Always 0 
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-int n;
+	srand(time(0));
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n > 1)
-printf("%d is positive\n", n);
-else if (n < 0)
-printf("%d is negative\n", n);
-else
+	int n = rand();
 
-printf("%d is zero\n", n);
-
-return (0);
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
 }
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,1 @@
-CONDITIONAL OPERATORS IN C
+This is a directory that deals about variables if else statementsa and aslo while statements
