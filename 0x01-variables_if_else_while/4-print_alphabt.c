0c9a8fc4d9b0c827bea30fea124272a1ab4b8c92
lijsamuael <lijsamuael@gmail.com>
2022-09-09 12:25:31 -0700

    adding task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		if (n == 113 || n == 101)
+		{
+		}
+		else
+		{
+			putchar(n);
+		}
+		n++;
+
+	}
+	return (0);
+}
