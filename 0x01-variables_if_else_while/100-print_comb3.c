50d981dc08282fbca30670d3ffc13ab43be52110
lijsamuael <lijsamuael@gmail.com>
2022-09-09 14:54:14 -0700

    adding task 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	int j;
+
+	for (i = 0; i <= 8; i++)
+	{
+		for (j = 1; j <= 9; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i != 8 || j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
