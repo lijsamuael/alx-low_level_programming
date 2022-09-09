8b6a2316032ea6aaff1556ce1767265de0eed745
lijsamuael <lijsamuael@gmail.com>
2022-09-09 13:36:44 -0700

    adding task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+	}
+	return (0);
+}
