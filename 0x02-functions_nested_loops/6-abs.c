687d51d8e6a043a855fec8cbc9d2b4a0d7a6be01
lijsamuael <lijsamuael@gmail.com>
2022-09-13 08:17:24 -0700

    adding task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - pirntsints theabsolute functio of the numebr
+ * @n: the number to be evvaluated
+ * Return: the abslolute value of the number
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,4 +3,5 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int);
 int _isalpha(int);
-int print_sign(int); 
+int print_sign(int);
+int _abs(int);
