c312d2282708c5c4a29a5858fef5b704a62ca84d
lijsamuael <lijsamuael@gmail.com>
2022-09-13 10:18:59 -0700

    correcting task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,7 +12,13 @@ int print_last_digit(int n)
 
 	last_digit = n % 10;
 	if (last_digit < 0)
+	{
+		_putchar(-last_digit);
 		return (-last_digit);
+	}
 	else
+	{
+		_putchar(last_digit);
 		return (last_digit);
+	}
 }
