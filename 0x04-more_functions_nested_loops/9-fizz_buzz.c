c3168d22aa93095d98373983f58df34436afbef0
lijsamuael <lijsamuael@gmail.com>
2022-09-15 13:23:50 -0700

    adding task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry of the program
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+			printf("FizzBuzz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else
+			printf("%d%s", i, " ");
+	}
+	return (0);
+}
