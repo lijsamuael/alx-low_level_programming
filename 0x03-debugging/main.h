72f2bbb9b1a76e4c58434480d002a9a4927f95fc
lijsamuael <lijsamuael@gmail.com>
2022-09-15 00:59:56 -0700

    adding task 3

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,7 +5,7 @@
 
 void positive_or_negative(int i);
 int largest_number(int, int, int);
-int convert_day(int, int);
 void print_remaining_days(int, int, int);
+int convert_day(int, int);
 
 #endif /* MAIN_H */
