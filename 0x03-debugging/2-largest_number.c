762b685da332fd68a6772fb7b307e1ec7123d548
lijsamuael <lijsamuael@gmail.com>
2022-09-15 01:37:33 -0700

    correcting task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,13 +16,17 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
+	else if (a > b && c > a)
 	{
-		largest = b;
+		largest = c;
+	}
+	else if (b > c)
+	{
+	largest = b;
 	}
 	else
 	{
-		largest = c;
+	largest = c;
 	}
 
 	return (largest);
