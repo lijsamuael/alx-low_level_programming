6026499109fe5891dfc6be5ce9787ec698424107
lijsamuael <lijsamuael@gmail.com>
2022-09-15 15:07:08 -0700

    fixing task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,10 +13,10 @@ int main(void)
 	{
 		if (i % 15 == 0)
 			printf("FizzBuzz ");
-		else if (i % 5 == 0)
-			printf("Buzz ");
 		else if (i % 3 == 0)
 			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
 		else
 			printf("%d%s", i, " ");
 	}
