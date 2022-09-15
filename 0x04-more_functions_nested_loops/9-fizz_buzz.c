ded6a61b9fd0857d055f4046aae21465ba3146c3
lijsamuael <lijsamuael@gmail.com>
2022-09-15 15:13:35 -0700

    oppppps

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,14 +15,16 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 15 == 0)
-			printf("FizzBuzz ");
-		else if (i % 3 == 0)
-			printf("Fizz ");
-		else if (i % 5 == 0)
-			printf("Buzz ");
-		else
-			printf("%d%s", i, " ");
+	if (i % 3 == 0 && i % 5 == 0)
+	printf("FizzBuzz ");
+	else if (i % 3 == 0 && i % 5 != 0)
+	printf("Fizz ");
+	else if (i == 100)
+	printf("Buzz");
+	else if (i % 5 == 0 && i % 3 != 0)
+	printf("Buzz ");
+	else
+	printf("%d ", i);
 	}
 	printf("\n");
 	return (0);
