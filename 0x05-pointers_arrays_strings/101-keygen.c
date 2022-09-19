6bcbdc9eb5ce6dd88e2c1a6302e6e581ff572917
lijsamuael <lijsamuael@gmail.com>
2022-09-19 11:44:36 -0700

    adding task 101

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcpy - copy the string
+ * @dest: destination string
+ * @src: the source string wichin is to be copied
+ * Return: a copied cahracter
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (dest);
+}
