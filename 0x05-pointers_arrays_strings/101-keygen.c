1b7667d811d33135f365ca168ea86b8efed034e4
lijsamuael <lijsamuael@gmail.com>
2022-09-19 11:52:07 -0700

    adding advanced task 2

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,13 +1,15 @@
 #include "main.h"
-#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 /**
- * _strcpy - copy the string
- * @dest: destination string
- * @src: the source string wichin is to be copied
- * Return: a copied cahracter
  */
-char *_strcpy(char *dest, char *src)
+int main(void)
 {
-	strcpy(dest, src);
-	return (dest);
+int num;
+
+srand(time(0));
+num = rand();
+printf("%i\n", num);
+return (0);
 }
