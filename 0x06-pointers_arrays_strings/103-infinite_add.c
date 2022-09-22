2005dfd2b7549ada2e56473ee6049ff09e9ec4ef
lijsamuael <lijsamuael@gmail.com>
2022-09-22 14:07:36 -0700

    adding the advanced task 4

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,11 +1,13 @@
 #include "main.h"
+
 /**
  * infinite_add - adds two numbers
- * @n1: number one.
- * @n2: number two.
- * @r: buffer that the function will use to store the result.
- * @size_r: buffer size:
- * Return: the pointer to dest.
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer for result
+ * @size_r: buffer size
+ *
+ * Return: address of r or 0
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
