c17a7bea7f69398d2407d71152cc450325f98af7
lijsamuael <lijsamuael@gmail.com>
2022-09-30 05:11:47 -0700

    adding task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - the entry of the program
+ * @argc: the number of an argument
+ * @argv: the array of arguments
+ * Return: integer
+ */
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
