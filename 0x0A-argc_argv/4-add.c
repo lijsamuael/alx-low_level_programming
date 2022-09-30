e7ddf45a113cea6cd60e09c499853bb539105d16
lijsamuael <lijsamuael@gmail.com>
2022-09-30 14:15:16 -0700

    correcting task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 /**
- * main - the entryof the program
- * @argc: the numberof the arguments
- * @argv: the array of the arguments
- * Return: an integer
+ * main - adds positive numbers
+ * @argc: argument count
+ * @argv: argument vector for values
+ * Return: always 0
  */
+
 int main(int argc, char *argv[])
 {
-	int i, sum;
+	int i, j, sum;
 
 	sum = 0;
 
-	for (i = 1; i < argc; i++)
+	for (i = 1; i < argc ; i++)
 	{
-		if (!isdigit(argv[i]))
+		for (j = 0; argv[i][j] != '\0' ; j++)
 		{
-			printf("%s\n", "Error");
-			return (1);
+			if (argv[i][j] < 47 || argv[i][j] > 57)
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
-		sum += atoi(argv[i]);
+		sum = sum + atoi(argv[i]);
 	}
 	printf("%d\n", sum);
 	return (0);
