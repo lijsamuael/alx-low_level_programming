adfc51d0ebae2f1ed4d7ad56affe25daf94580f3
lijsamuael <lijsamuael@gmail.com>
2022-10-10 14:55:21 -0700

    adding project about preprocessor

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,6 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+#define PI 3.14159265359
+
+#endif
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main- prints file name
+ *
+ *Return: Always 0
+ */
+
+int main(void)
+{
+
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
