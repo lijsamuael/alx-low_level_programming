16c829b8b30289d5bacac1e15c0330cb5110fb7a
lijsamuael <lijsamuael@gmail.com>
2022-10-10 06:25:23 -0700

    sageguarding header files

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * struct dog - a structure of dog
+ * @name: name of the dog
+ * @age: the age of the dog
+ * @owner: the ownerof the dog
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif /* MAIN_H */
--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -1,5 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 int _putchar(char);
+
+#endif /* MAIN_H */
