55607b3d36a1976be175fc364b2a39f7e72346f0
lijsamuael <lijsamuael@gmail.com>
2022-10-10 11:54:36 -0700

    adding a function that prints the elements of the structure

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,14 +10,14 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		if (d->name == NULL)
-			printf("Name: nil");
+			printf("Name: (nil)");
 		else
 			printf("Name: %s\n", d->name);
 
 		printf("Age: %f\n", d->age);
 
 		if (d->owner == NULL)
-			printf("Owner: nil");
+			printf("Owner: (nil)");
 		else
 			printf("Owner: %s\n", d->owner);
 	}
