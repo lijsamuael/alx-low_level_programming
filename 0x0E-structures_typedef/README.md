16fc258c7502d98e644f2a028cb69aabbe561e44
lijsamuael <lijsamuael@gmail.com>
2022-10-10 06:02:52 -0700

    adding header file _putchar and readme files

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+a directory for structure and typedof projects
---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,5 @@
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+int _putchar(char);
