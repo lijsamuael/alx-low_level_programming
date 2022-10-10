37bd5c99d1229bb6c680f599cbf086098742cd49
lijsamuael <lijsamuael@gmail.com>
2022-10-10 12:13:32 -0700

    making another name for the structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,7 +12,7 @@ struct dog
 	float age;
 	char *owner;
 };
-typedof struct dog dog_t;
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
