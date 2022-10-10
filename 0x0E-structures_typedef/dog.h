437cfb79537ae5eec3cbb683dcf647615fac7b17
lijsamuael <lijsamuael@gmail.com>
2022-10-10 11:03:55 -0700

    adding a fumction that initializes a structure pointer

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,4 +13,11 @@ struct dog
 	char *owner;
 };
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+int _putchar(char);
+
+
 #endif /* MAIN_H */
