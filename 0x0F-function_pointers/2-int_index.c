dc681183dff84d9559995717fbd0ee842e02c5f2
lijsamuael <lijsamuael@gmail.com>
2022-10-12 12:54:56 -0700

    adding task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include 
+#include "function_pointers.h"
+/**
+ * int_index - searchs the index for the giben number
+ * @array: the array of numbers
+ * @size: the size of the array
+ * @cmp: a function to compare the elements in the array
+ * Return: the index of the mathced number
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int count;
+
+	count = 0;
+	if (size <= 0)
+		return (-1);
+	if (array != NULL && cmp != NULL)
+	{
+		int i;
+
+		for (i = 0; i < size; i++)
+		{
+			if (count != 0)
+				return (i - 1);
+			count = (cmp(array[i]));
+		}
+	}
+	return (-1);
+}
