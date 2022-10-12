24b92b9b1e7a64a883799501b3808a744fb5a70b
lijsamuael <lijsamuael@gmail.com>
2022-10-12 12:10:37 -0700

    adding the second task

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stddef.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - prints the elements in the array
+ * @array: an array
+ * @size: the size of the array
+ * @action: a function
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array != NULL && action != NULL)
+	{
+		long unsigned int i;
+
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
