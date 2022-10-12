d9aa39fc03e695887f89123f4ba47895a80e9431
lijsamuael <lijsamuael@gmail.com>
2022-10-12 11:45:51 -0700

    adding the first task

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name - prints a name
+ * @name: the name to be pritned
+ * @f: a function
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
