8d483bf0f97f0f93166e462a69ff86048127ebb1
lijsamuael <lijsamuael@gmail.com>
2022-10-24 14:04:53 -0700

    adding a file taht frees a linked list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint - free a linked list
+ * @head: the start node of the linked list
+ * Return: none
+ */
+void free_listint(listint_t *head)
+{
+	if (head == NULL)
+		;
+	else
+	{
+		listint_t *temp;
+
+		while (temp != NULL)
+		{
+			temp = head;
+			head = head->next;
+			free(temp);
+		}
+	}
+}
