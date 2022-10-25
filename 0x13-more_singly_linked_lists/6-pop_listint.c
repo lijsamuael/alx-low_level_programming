b1af0b3c68808e08f9fa17ed7d9cf46fd9116925
lijsamuael <lijsamuael@gmail.com>
2022-10-25 11:17:27 -0700

    adding a file that delets the head of a node

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * pop_listint - delets the first node of the linked list
+ * @head: the first node in the linked list
+ * Return: the removed node;
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int i;
+
+	if (head == NULL)
+		return (0);
+	temp = *head;
+	if ((*head)->next == NULL)
+	{
+		i = temp->n;
+		free(temp);
+		head = NULL;
+
+	}
+	else
+	{
+		*head = temp->next;
+		i = temp->n;
+		free(temp);
+	}
+	return (i);
+
+}
